772498d536473cfb5246cb6f3bac349083be12f4
Nandha-hub <nandu.feb13@gmail.com>
2024-12-02 14:37:48 +0530

    Added DataTypes Program

---
+++ src/org/dataTypes/EmployeeDetails.java
@@ -0,0 +1,34 @@
+package org.dataTypes;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		byte age =22;
+		System.out.println("Employee Age:"+age);
+		
+		short pin=1234;
+		System.out.println("Employee Pin :"+pin);
+		
+		int regno=834678346;
+		System.out.println("Employee Registration Number:"+regno);
+		
+		long accNo=654345678786431234L;
+		System.out.println("Employee Account Number :"+accNo);
+		
+		float sal=89.7654345676543f;
+		System.out.println("Employee Salary :"+sal);
+		
+		double avg=98.543456786545;
+		System.out.println("Employee Average:"+avg);
+		
+		char gender ='M';
+		System.out.println("Employee Gender:"+gender);
+		
+		String email="sst123@gmail.com";
+		System.out.println("Employee Mail Id:"+email);
+		
+		boolean rsult=true;
+		System.out.println("All the Above Information are:"+rsult);
+		
+	}
+
+}
